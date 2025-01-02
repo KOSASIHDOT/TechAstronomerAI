3fd696f50b68e2b377e2e45e72ed6a403976c2e9
KOSASIH . <terminaloflife@gmail.com>
2025-01-02 10:40:13 +0000

    skernal upd

--- example_code.py
+++ example_code.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report 
 
 # Load the spectroscopic data
 data = pd.read_csv('spectroscopic_data.csv')
